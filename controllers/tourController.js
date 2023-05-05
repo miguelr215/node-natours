c5b548757db8d93169c7f2d16e0ce7e5489f7df9
miguelr215 <drexelramos@gmail.com>
2023-05-04 20:14:25 -0700

    refactored updateTour & deleteTour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -114,22 +114,38 @@ exports.createTour = async (req, res) => {
   // );
 };
 
-exports.updateTour = (req, res) => {
-  // GET TOUR, UPDATE TOUR, SAVE TOUR CODE HERE
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>',
-    },
-  });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: `Error updating tour: ${error}`,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  // DELETE TOUR CODE HERE
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: `Error deleting tour: ${error}`,
+    });
+  }
 };
