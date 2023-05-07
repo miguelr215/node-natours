7f6fdf874b0f9590e18cb4ed0c47abcd3a130415
miguelr215 <drexelramos@gmail.com>
2023-05-07 05:40:44 -0700

    added queryObj

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,11 +29,36 @@ const Tour = require('./../models/tourModel');
 // };
 
 exports.getAllTours = async (req, res) => {
-  // console.log(req.requestTime);
-
   try {
-    const tours = await Tour.find();
+    // console.log(req.requestTime);
 
+    // console.log(req.query);
+
+    // BUILD THE QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+    // console.log(queryObj);
+
+    // FILTER METHOD #1
+    // const tours = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy',
+    // });
+
+    // FILTER METHOD #2
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    const query = Tour.find(queryObj);
+
+    // EXECUTE THE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
